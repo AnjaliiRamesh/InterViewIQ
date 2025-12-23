b772e3a4b8cd12befa56074323fec7b4547678e4
Anjali <anjaliramesh14012005@gmail.com>
2025-12-23 15:00:18 +0530

    commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,26 @@
+
+import './App.css'
+import { SignedIn, SignedOut, SignInButton, SignOutButton,UserButton } from '@clerk/clerk-react'
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1>Welcome Anjali</h1>
+    <SignedOut>
+   <SignInButton mode='modal'>
+    <button>Login</button>
+   </SignInButton>
+    </SignedOut>
+
+    <SignedIn>
+      <SignOutButton />
+    </SignedIn>
+
+    <UserButton/>
+    </>
+  )
+}
+
+export default App
